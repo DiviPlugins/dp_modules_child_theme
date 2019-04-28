b79964b2f41beb0500d05ab77613ca5c2f91691b
Carlos Rafael Garcia Caballero <charles890123@gmail.com>
2019-04-28 12:13:11 -0400

    Improve endpoint callbacks

--- functions.php
+++ functions.php
@@ -24,30 +24,45 @@ add_filter('upload_mimes', 'custom_myme_types', 1, 1);
 function api_endpoint_for_modules_data() {
     register_rest_route('divi_plugins/v1', '/modules', array(
         array('method' => 'GET', 'callback' => 'get_custom_modules_info'),
-        array('method' => 'POST', 'callback' => 'add_new_module')
+            //array('method' => 'POST', 'callback' => 'add_new_module')
     ));
 }
 
 function get_custom_modules_info() {
     $all_modules_info = array();
     $query = new WP_Query(array(
-        'post_type' => 'post',
-        'post_status' => 'publish',
+        'post_type' => "download",
+        'post_status' => "publish",
         'posts_per_page' => -1,
-        'cat' => array(2, 3)
+        'tax_query' => array(
+            array(
+                'taxonomy' => "download_category",
+                'terms' => array(2, 3),
+                'operator' => "IN"
+            )
+        )
     ));
     if ($query->have_posts()) {
         while ($query->have_posts()) {
             $query->the_post();
             $data['id'] = get_the_ID();
+            $file_info = edd_get_download_files($data['id']);
             $data['name'] = get_the_title();
+            $data['thumbnail'] = get_the_post_thumbnail_url($data['id'], "medium");
             $data['description'] = get_the_excerpt();
-            $data['category'] = has_category(2) ? 'module' : 'user-module';
-            $data['file_content'] = file_get_contents(get_field('module_file')['url']);
-            array_push($all_modules_info, $data);
+            $data['link'] = get_the_permalink();
+            $data['category'] = has_term(2, "download_category") ? 'module' : 'user-module';
+            if (is_array($file_info)) {
+                $file_url = $file_info[1]["file"];
+                if (!empty($file_url)) {
+                    $data['file_content'] = file_get_contents($file_url);
+                }
+                $all_modules_info[$data['id']] = $data;
+            }
         }
+        $all_modules_info['api_query_errors'] = "OK";
     } else {
-        $all_modules_info = array('api_query_errors' => 'There are no custom modules published at the moment.');
+        $all_modules_info['api_query_errors'] = __('There are no custom modules published at the moment.');
     }
     wp_reset_postdata();
     $response = new WP_REST_Response($all_modules_info, 200);
